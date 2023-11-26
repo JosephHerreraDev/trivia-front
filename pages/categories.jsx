2d6de7f3686e4d920c32da306ac7e8c50bed9e6d
Joseph Herrera <josephherreraleon@gmail.com>
2023-11-25 20:07:07 -0600

    Add form submission functionality to login page

---
+++ pages/categories.jsx
@@ -0,0 +1,11 @@
+import "/src/app/globals.css";
+import "./categories.css";
+
+export default function Categories() {
+  return (
+    <div className="categories">
+      <h1>Categories</h1>
+      <categoryCard />
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,29 @@
+"use client";
+
 import Image from "next/image";
 import "./login.css";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleSubmit = (e) => {
+    router.push("/categories");
+  };
+
   return (
     <article className="login">
       <h1 className="login-title">Bienvenido 👋</h1>
       <p className="login-subtitle">Ingrese un usuario para comenzar</p>
-      <form className="login-form">
+      <form className="login-form" onSubmit={handleSubmit}>
         <div className="input-container">
           <input placeholder="usuario" type="text" id="username" required />
           <label htmlFor="username">Usuario</label>
         </div>
-        <Link className="login-submit" type="submit" href={"/si"}>
+        <button className="login-submit" type="submit">
           Ingresar
-        </Link>
+        </button>
       </form>
     </article>
   );
